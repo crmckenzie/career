62810a3750b470ce5dc76ebd8b714ce3445371bf
Chris McKenzie <cmckenzie@paraport.com>
2015-08-05 13:24:08 -0700

    moved .psundle dir outside of .psundle module

--- windows-workstation/Modules/Psundle/Functions/Get-PsundleModulesDirectory.ps1
+++ windows-workstation/Modules/Psundle/Functions/Get-PsundleModulesDirectory.ps1
@@ -1,20 +1,5 @@
 function Get-PsundleModulesDirectory{
-  $PsundleModule = Get-Module "Psundle"
-  $psundleLocation = ""
-
-<#
   $modulesPath = $env:PsModulePath.Split(';') | where {$_.Contains($home)}
-  $psundleModulesPath = "$modulesPath/.psundle"
-#>
-  $moduleIsLoading= $PsundleModule -eq $null
-  if ($moduleIsloading){
-    $dirInfo = New-Object "System.IO.DirectoryInfo" $PSScriptRoot
-    $psundleLocation = Join-Path $dirInfo.Parent.FullName "Psundle.psd1"
-  }
-  else {
-    $psundleLocation =  $PsundleModule.Path
-  }
-  $directory = (New-Object "System.IO.FileInfo" $psundleLocation).Directory
-  $psundleModules = "$directory\.PsundleModules"
+  $psundleModules = "$modulesPath/.psundle"
   return $psundleModules
 }
