3023c82abe65521f91e05ac7c28e857e3a3a67c1
crmckenzie <crmckenzie@gmail.com>
2017-03-13 20:16:56 -0700

    updating powershell profile for the modern era

--- windows-workstation/Install-Profile.ps1
+++ windows-workstation/Install-Profile.ps1
@@ -1,49 +1,23 @@
-(new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
-
-$documents = "$home\Documents"
+$documents = [Environment]::GetFolderPath("MyDocuments")
 $powershellDir = "$documents\WindowsPowershell"
 $moduleDir = "$powershellDir\Modules"
 
-write-host "Installing Posh-Git"
-Install-Module Posh-Git
-write-host "Posh-Git installed."
-
-write-host "Installing PSCX"
-Invoke-WebRequest "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=pscx&DownloadId=923562&FileTime=130585918034470000&Build=21028" -OutFile pscx.msi
-msiexec /i pscx.msi /q
-remove-item pscx.msi -force
-write-host "PSCX Installed"
 
 $files = [System.IO.Directory]::GetFiles("$PSSCriptRoot\Profiles")
 $files | foreach {
-  $file = New-Object "System.IO.FileInfo" $_
-  $filename = $file.Name
-  $fullFileName = $file.FullName
+    $file = New-Object "System.IO.FileInfo" $_
 
-  $symlink = "$powershellDir\$filename"
-  if ([System.IO.FIle]::Exists($symlink)){
-    write-debug "Removing old symlink for $symlink"
-    [System.IO.File]::Delete($symlink)
-  }
+    $symlink = "$powershellDir\$($file.Name)"
+    if ([System.IO.FIle]::Exists($symlink)){
+        write-debug "Removing old symlink for $symlink"
+        [System.IO.File]::Delete($symlink)
+    }
 
-  $cmd = "New-Symlink ""$symlink"" ""$fullFileName"""
-  $cmd
-  Invoke-Expression $cmd
-}
+    if (-not (Test-Path $powershellDir)) {
+        write-host "Creating the powershell directory at $powershellDir."
+        New-Item -ItemType Directory -Path $powershellDir -Force
+    }
 
-$directories = [System.IO.Directory]::GetDirectories("$PSScriptRoot\Modules")
-$directories | foreach {
-  $directoryInfo = New-Object "System.IO.DirectoryInfo" $_
-  $directoryName = $directoryInfo.Name
-  $target = "$powershellDir\Modules\$directoryName"
-
-  if ([System.IO.Directory]::Exists($target)) {
-    $cmd = "cmd.exe /c 'rmdir $target'"
-    $cmd
-    Invoke-Expression $cmd
-  }
-
-  $cmd = "New-SymLink ""$target"" ""$_"""
-  $cmd
-  Invoke-Expression $cmd
+    write-host "Creating symbolic link $powershellDir\$($file.Name) => $($file.FullName)."
+    New-Item -ItemType SymbolicLink -Path $powershellDir -Name $file.Name -Value $file.FullName
 }
