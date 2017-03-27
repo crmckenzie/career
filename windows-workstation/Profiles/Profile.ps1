91614ff0a4edc3fdac10a17347da3904bf9f7234
crmckenzie <crmckenzie@gmail.com>
2017-03-27 09:55:32 -0700

    adding module development directory to PsModulePath

--- windows-workstation/Profiles/Profile.ps1
+++ windows-workstation/Profiles/Profile.ps1
@@ -1,1 +1,49 @@
-Set-ExecutionPolicy BypassImport-Module PSCX$documents = "$home\Documents"$powershellDir = "$documents\WindowsPowershell"$moduleDir = "$powershellDir\Modules"# load all modules in Module root(gci $moduleDir) | foreach {gci $_.FullName *.psm1} | Import-Module$code = (gci 'C:\Users\ChrisMc\AppData\Local\Code\**\code.exe')[-1].FullName$atom = (gci 'C:\Users\ChrisMc\AppData\Local\atom\**\atom.exe')[-1].FullNameset-alias np "C:\Program Files (x86)\Notepad++\Notepad++.exe"set-alias x explorerset-alias which where.exeset-alias nant "C:\Program Files\Nant\bin\Nant.exe"set-alias vim 'C:\git\bin\vim\vim.exe'set-alias sqlcompare 'C:\Program Files (x86)\Red Gate\SQL Compare 11\sqlcompare.exe'set-alias code $codeset-alias atom $atomremove-item alias:\curlwrite-host "Powershell Initialized. Ready... Set... GO!!!"
+Set-ExecutionPolicy RemoteSigned
+
+Get-Module -ListAvailable "posh-git" | Import-Module
+if (Get-Module posh-git) {
+    function global:prompt {
+        $realLASTEXITCODE = $LASTEXITCODE
+
+        Write-Host($pwd.ProviderPath) -nonewline
+
+        Write-VcsStatus
+
+        $global:LASTEXITCODE = $realLASTEXITCODE
+        return "> "
+    }
+
+    Pop-Location
+
+    Start-SshAgent -Quiet    
+}
+
+set-alias np "C:\Program Files (x86)\Notepad++\Notepad++.exe"
+set-alias x explorer
+set-alias which where.exe
+
+remove-item alias:\curl
+
+$My = [PsCustomObject] @{
+	"Octopus" = @{
+		"ApiKeys" = @{
+			"Read" = "API-XD7DGUGJ8HBK9JR9WFYCOCTXVM";
+			"Write" = "API-LNVERZ5FP3TM2ARHSYEF6ZLDVLQ";
+		}
+	}
+}
+
+# Set powershell module development path to PsModulePath
+$devModulePath = "$($env:USERPROFILE)\src\Modules"
+if (-not (($env:PSModulePath -split ';') -contains $devModulePath)) {
+    write-host "Adding module development directory ($devModulePath) to `$env:PsModulePath."
+    $CurrentValue = [Environment]::GetEnvironmentVariable("PSModulePath", "User")
+    if ($CurrentValue) {
+        [Environment]::SetEnvironmentVariable("PSModulePath", "$CurrentValue;$devModulePath", "User")
+    } else {
+        [Environment]::SetEnvironmentVariable("PSModulePath", "$devModulePath", "User")        
+    }
+    $env:PsModulePath += ";$devModulePath"
+}
+
+write-host "Powershell Initialized. Ready... Set... GO!!!"
